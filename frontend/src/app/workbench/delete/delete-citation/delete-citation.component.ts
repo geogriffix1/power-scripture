4bf29d01c43bd18480da05d121e127a420e451a7
George Griffin <geogriffix@gmail.com>
2026-04-30 20:29:31 -0500

    Trying again.

--- frontend/src/app/workbench/delete/delete-citation/delete-citation.component.ts
+++ frontend/src/app/workbench/delete/delete-citation/delete-citation.component.ts
@@ -1,5 +1,4 @@
-import { OnInit, Input, Signal } from '@angular/core';
-import { Component } from '@angular/core';
+import { Component, Input, Signal, effect } from '@angular/core';
 import { BibleService } from '../../../bible.service';
 import { ThemeToCitationModel } from '../../../model/themeToCitation.model';
 import { JstreeModel } from '../../../model/jstree.model';
@@ -19,112 +18,60 @@ export class DeleteCitationComponent {
   static isActive: boolean;
   static isSubscribed: boolean;
   sectionWidth?: number;
-  missingMessage = "Please select an active <b>Citation</b> from the <b>Bible Theme Tree</b>";
+  missingMessage = "Please select the active <b>Citation</b> to delete from the <b>Bible Theme Tree</b>";
 
-  constructor(private service: BibleService) {}
+  constructor(private service: BibleService)
+  {
+    console.log("DeleteCitationComponent ctor");
+     effect(() => {      
+      $(".command-message, .command-warning").text("").hide(100);
+      if (this.activeCitationNode()) {
+        console.log("activeCitationNode changed");
+        console.log(this.activeCitationNode());
+        let path = BibleThemeTreeComponent.getDomNode(this.activeCitationNode()!.parent).data.path;
+        console.log("path: ", path);
+        let id = <number><unknown>this.activeCitationNode()?.id?.replace("citation", "");
+        let description = this.activeCitationNode()!.li_attr.title;
+
+        $("#label").val(this.activeCitationNode()!.text ?? "")
+          .attr("title", this.activeCitationNode()!.text ?? "[empty description]")
+          .show(500);
+        $("#description")
+          .val(description.trim() == "" ? "[empty description]" : description)
+          .show(500);
+        $("div.citation.selected").removeClass("missing").text(path);
+      }
+      else {
+        $("#label").val("");
+        $("#description").val("").attr("title", "");
+        $("div.citation.selected")
+          .removeClass("missing")
+          .addClass("missing")
+          .html(this.missingMessage);
+      }
+    });
+  }
 
   DeleteCitationLink(e?: Event) {
     e?.preventDefault();
     e?.stopPropagation();
 
+    $(".command-warning").hide(100);
+    $(".command-message").text("");
+
     console.log("DeleteCitationLink clicked");
-    console.log(`this.activeThemeToCitation.id: ${this.activeThemeToCitation.id}`);
     (async() => {
-      const themeId = this.activeThemeToCitation.themeId;
-      await this.service.deleteThemeToCitation(this.activeThemeToCitation.id);
+      const themeId = +this.activeCitationNode()!.parent.replace("theme", "");
+      const themeToCitationId = +this.activeCitationNode()!.id.replace("citation", "");
+      await this.service.deleteThemeToCitation(themeToCitationId);
       BibleThemeTreeComponent.refreshDomNodeFromDb(`theme${themeId}`);
       WorkbenchComponent.activeCitation = JstreeModel.null;
       $("#label").val("");
-      $("#citation-description").val("");
+      $("#description").val("").attr("title", "");
       $("div.citation.selected").addClass("missing").html(this.missingMessage);
-      $(".command-warning").show(100);
-      $(".command-message").text("Delete successful");
+      $(".command-warning").text("Delete successful").show(100);
     })();
 
     $("command-message").text("");
-  }
-
-  ngOnInit() {
-    DeleteCitationComponent.isActive = true;
-  }
-  
-  ngAfterViewInit() {
-    console.log("ngAfterViewInit");
-    if (!DeleteCitationComponent.isSubscribed) {  
-      (async (obj:DeleteCitationComponent) => {
-        console.log("subscribing to active citation selector");
-        BibleThemeTreeComponent.ActiveCitationSelector
-          .subscribe((node:any) => {
-            if (WorkbenchComponent.activeCitation) {
-              console.log("WorkbenchComponent.activeCitation");
-              console.log(WorkbenchComponent.activeCitation);
-              let parent = WorkbenchComponent.activeCitation.parent;
-              let parentNode = BibleThemeTreeComponent.getDomNode(parent);
-              console.log("parentNode:");
-              console.log(parentNode);
-              console.log(`parentNode.data.path ${parentNode.data.path}`);
-              if (parent.startsWith("theme")) {
-                parent = parent.replace("theme", "");
-              }
-              else {
-                parent = "0";
-              }
-
-              this.activeThemeToCitation = {
-                id: <number><unknown>WorkbenchComponent.activeCitation.id.replace("citation", ""),
-                citationLabel: WorkbenchComponent.activeCitation.text,
-                description: WorkbenchComponent.activeCitation.li_attr.title,
-                citationId: WorkbenchComponent.activeCitation.li_attr.citationId,
-                themeId: +parent,
-                sequence: WorkbenchComponent.activeCitation.li_attr.sequence,
-              };
-
-              $("#label").val(this.activeThemeToCitation.citationLabel);
-              $("#citation-description").val(obj.activeThemeToCitation.description);
-              $("div.citation.selected").removeClass("missing").text(parentNode.data.path).show(500);
-              $(".command-warning").hide();
-              $(".commandMessage").text("");
-            }
-            else {
-              $("div.citation.selected").addClass("missing");
-            }
-          });
-      })(this);
-
-      DeleteCitationComponent.isSubscribed = true;
-    }
-
-    (async (obj:DeleteCitationComponent) => {
-      if (WorkbenchComponent.activeCitation) {
-        console.log("WorkbenchComponent has an active citation");
-        let parent = WorkbenchComponent.activeCitation.parent;
-        let parentNode = BibleThemeTreeComponent.getDomNode(parent);
-        if (parent.startsWith("theme")) {
-          parent = parent.replace("theme", "");
-        }
-        else {
-          parent = "0";
-        }
-
-        this.activeThemeToCitation = {
-          id: <number><unknown>WorkbenchComponent.activeCitation.id.replace("citation", ""),
-          citationLabel: WorkbenchComponent.activeCitation.text,
-          description: WorkbenchComponent.activeCitation.li_attr.title,
-          citationId: WorkbenchComponent.activeCitation.li_attr.citationId,
-          themeId: +parent,
-          sequence: WorkbenchComponent.activeCitation.li_attr.sequence,
-        };
-
-        $("#label").val(this.activeThemeToCitation.citationLabel);
-        $("#citation-description").val(obj.activeThemeToCitation.description);
-        $("div.citation.selected").removeClass("missing").text(parentNode.data.path).show(500);
-        $(".command-warning").hide();
-        $(".command-message").text("");
-      }
-      else {
-        console.log("No WorkbenchComponent.activeCitation");
-        $("div.selected.citation").html(this.missingMessage).addClass("missing");
-      }
-    })(this);
   }
 }
